33712b12c3bd933a03902dc3a928017d04730944
aarthikrishnamoorthy <47214239+aarthikrishnamoorthy@users.noreply.github.com>
2019-02-11 14:26:18 +0530

    Update largest no.py

--- largest no.py
+++ largest no.py
@@ -1,7 +1,7 @@
 n1,n2,n3=map(int,input().split())
 if n1>n2 and n1>n3:
-    print(n1,"is the largest no")
+    print(n1)
 elif n2>n3:
-    print(n2,"is the largest no")
+    print(n2)
 else:
-    print(n3,"is the largest no")
+    print(n3)
